dfcddd1af39ab2d1ca0b458810643bc74cc56eb1
FelipeFama <lipehfama@gmail.com>
2024-05-06 22:30:53 -0300

    feat: add carrousel feature in teachers section

--- js/script.js
+++ js/script.js
@@ -66,3 +66,20 @@ let swiperCourses = new Swiper(".home-courses-slider", {
     },
   },
 });
+
+let swiperTeachers = new Swiper(".teachers-slider", {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 20,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    991: {
+      slidesPerView: 3,
+    },
+  },
+});
