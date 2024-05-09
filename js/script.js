e8f658f9fa5d603bff68d36195dc7f9d07276e8d
FelipeFama <lipehfama@gmail.com>
2024-05-09 02:05:43 -0300

    change script

--- js/script.js
+++ js/script.js
@@ -73,10 +73,7 @@ accordion.forEach((item) => {
 let loading = document.querySelector("#loading-page");
 
 function loadingPage() {
-  if (
-    window.location.href.includes("home") ||
-    window.location.href.includes("/")
-  ) {
+  if (window.location.href.includes("home")) {
     loading.classList.add("active");
   }
 }
