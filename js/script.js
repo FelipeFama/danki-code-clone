1d06eb767906d3e0afb20d74e646ffbd741c3abe
FelipeFama <lipehfama@gmail.com>
2024-05-08 22:46:27 -0300

    feat: styles in carrosel feature in faq section

--- js/script.js
+++ js/script.js
@@ -129,3 +129,26 @@ let swiperReviews = new Swiper(".reviews-slider", {
     },
   },
 });
+
+let swiperLogo = new Swiper(".logo-slider", {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 20,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    450: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    991: {
+      slidesPerView: 4,
+    },
+    1200: {
+      slidesPerView: 5,
+    },
+  },
+});
