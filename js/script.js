e0881cbd06fd5037c0d23f8fba4c2b04f9607cd8
FelipeFama <lipehfama@gmail.com>
2024-05-09 02:37:31 -0300

    change js

--- js/script.js
+++ js/script.js
@@ -74,8 +74,8 @@ let loading = document.querySelector("#loading-page");
 
 function loadingPage() {
   if (window.location.href.includes("home")) {
+    loading.classList.add("active");
   }
-  loading.classList.add("active");
 }
 
 function fadeOut() {
